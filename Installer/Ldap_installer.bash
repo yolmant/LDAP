fe481754f9c86e5bfc11aa187c30b772e3d2182d
yolmant <yolmat24@outlook.com>
2017-02-16 03:11:24 -0800

    Update Ldap_installer.bash

--- Installer/Ldap_installer.bash
+++ Installer/Ldap_installer.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+#this is the multi-tasker. Install and configure the LDAP server
 Menu=$(whiptail --title "LDAP" --menu "Choose an option" 15 60 5 \
 "1" "Install LDAP packages" \
 "2" "configure LDAP server" \
@@ -6,28 +7,39 @@ Menu=$(whiptail --title "LDAP" --menu "Choose an option" 15 60 5 \
 "4" "create users" \
 "5" "Uninstall LDAP" 3>&1 1>&2 2>&3)
 
+#check if the user press "ok" or "cancel"
 exitstatus=$?
 
+#if the user pressed "ok"
 if [ $exitstatus = 0 ]; then
+#install package selected
 	if [ $Menu = 1 ]; then
 		{
+			#a Bar loading that charge from 0% to 100% by 20%
     			for ((i = 0 ; i <= 120 ; i+=20)); do
         			if [ $i = 20 ]; then
+					#install the LDAP server tools
 					yum -y install openldap-servers
 				elif [ $i = 40 ]; then
+					#install the LDAp client tools
 					yum -y install openldap-clients
 				elif [ $i = 60 ]; then
+					#install the apache server
 					yum -y install httpd
 				elif [ $i = 80 ]; then
+					#install the EPEL package
 					yum -y install epel-release
 				elif [ $i = 100 ]; then
+					#Install the PHP LDAP package
 					yum -y install phpldapadmin
 				fi
 				echo $i
 				sleep 1
  			done 
+		#show a dialog box of the bar changing
 		} | whiptail --gauge "Please wait while installing" 6 60 0
-	
+		
+		#dialog box
 		whiptail --title "LDAP Installer" --msgbox "Packages installed. press ok to continue" 10 60
 		{	
 			systemctl enable slapd.service
@@ -35,34 +47,48 @@ if [ $exitstatus = 0 ]; then
         		systemctl enable httpd.service
         		systemctl start httpd.service
 		
+		#dialog box while the services are enabling and starting
 		} | whiptail --title "LDAP Installer" --msgbox "enabling and starting services" 10 60
 		
 		{
-        		#sed -i 's,Require local,#Require local\n    Require all granted,g' /etc/httpd/conf.d/phpldapadmin.conf
-
+			#modify the file phpldapadmin to permit the connection troughout the website
+        		sed -i 's,Require local,#Require local\n    Require all granted,g' /etc/httpd/conf.d/phpldapadmin.conf
+			
+			#restart the httpd service to save the changes
         		systemctl restart httpd.service
-
+			
+			#tell Linux what is going on
         		setsebool -P httpd_can_connect_ldap on
-      
+      		
+		#dialog box
 		} | whiptail --title "LDAP Installer" --msgbox "services started and enabled" 10 60
-
+	
+	#configuration of LDAP server
 	elif [ $Menu = 2 ]; then
+		#create the directory where the ldif files will be stored
 		mkdir /tmp/LDAP.cfg
+		#dialog box
 		whiptail --title "LDAP configuration" --msgbox "this configuration will automatically setup in the LDAP server and any ldif file will be stored in the next directory /tmp/LDAP.cfg" 10 60
-
+			#dialog box asking for the domain
 			Domain=$(whiptail --title "LDAP configuration" --inputbox "please introduce the domain or distinguished name. for example:" 10 60 dc=example,dc=net 3>&1 1>&2 2>&3)
 			option=$?
 			Do=$(echo $Domain | awk -F[=,] '{print $2}')
+			#if the user pressed "ok"
 			if [ $option = 0 ]; then
+				#dialog box asking for the domain of the root
 				RootD=$(whiptail --title "LDAP configuration" --inputbox "please introduce the LDAP account for root. for example:" 10 60 cn=admin,dc=example,dc=net 3>&1 1>&2 2>&3)
 				option=$?
 				RD=$(echo $RootD | awk -F[=,] '{print $2}')
+				#user pressed "ok"
 				if [ $option = 0 ]; then
+					#dialog box asking for the password of the administrator
 					Passwd=$(whiptail --title "LDAP configuration" --passwordbox "please introduce the LDAP root account password:" 10 60 3>&1 1>&2 2>&3)
 					Passw=$(slappasswd -s $Passwd -h {SSHA})
 					option=$?
+					#user pressed "ok"
 					if [ $option = 0 ]; then
 						{
+							#create a ldif file with the configuration of the administrator
 							sh -c 'cat > /tmp/LDAP.cfg/db.ldif' << EF
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
@@ -79,7 +105,7 @@ changetype: modify
 replace: olcRootPW
 olcRootPW: $Passw
 EF
-						
+							#ldif file allowing the access of the administrator
 							sh -c 'cat > /tmp/LDAP.cfg/monitor.ldif' << EF
 dn: olcDatabase={1}monitor,cn=config
 changetype: modify
@@ -87,7 +113,7 @@ replace: olcAccess
 olcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external, cn=auth" read by dn.base="$RootD" read by * none
 EF
 	
-							#creating a certification file
+							#creating a certification ldif file
 							sh -c 'cat > /tmp/LDAP.cfg/certs.ldif' << EF
 dn: cn=config
 changetype: modify
@@ -99,7 +125,7 @@ changetype: modify
 replace: olcTLSCertificateKeyFile
 olcTLSCertificateKeyFile: /etc/openldap/certs/LDAPkey.pem
 EF
-						
+							#creating a ldif file with the base configuration
 							sh -c 'cat > /tmp/LDAP.cfg/base.ldif' << EF
 dn: $Domain
 dc: $Do
@@ -119,35 +145,42 @@ dn: ou=Group,$Domain
 objectClass: organizationalUnit
 ou: Group
 EF
-
+						#dialog box
 						} | whiptail --title "LDAP configuration" --msgbox "creating file. wait a minute" 10 60
-
-						Co=$(whiptail --title "LDAP configuration" --inputbox "this prompt will create the LDAP certification for your server.\
+						
+						#asking the user information to create the LDAP certification 
+						#country
+						Co=$(whiptail --title "LDAP configuration" --inputbox "this will create the LDAP certification for your server.\
 
 please introduce the two initial of the country. for example:" 10 60 US 3>&1 1>&2 2>&3)
-
+						#state
 						St=$(whiptail --title "LDAP configuration" --inputbox "please introduce the two initial of the State. for example:" 10 60 WA 3>&1 1>&2 2>&3)
-
+						#city
 						ci=$(whiptail --title "LDAP configuration" --inputbox "please introduce the city. for example:" 10 60 Seattle 3>&1 1>&2 2>&3)
-
+						#Organization
 						Org=$(whiptail --title "LDAP configuration" --inputbox "please introduce the name of the Organization. for example:" 10 60 ITcorp 3>&1 1>&2 2>&3)
-
+						#Organization Unit
 						O=$(whiptail --title "LDAP configuration" --inputbox "please introduce the Organizational Unit. for example:" 10 60 ITinfra 3>&1 1>&2 2>&3)
-
+						#Common Name
 						Ca=$(whiptail --title "LDAP configuration" --inputbox "please introduce the Common Name. for example:" 10 60 server.NTI.local 3>&1 1>&2 2>&3)
 			
 						{
+							#creating the key and certification
 							openssl req -new -x509 -nodes -out /etc/openldap/certs/LDAPcert.pem -keyout /etc/openldap/certs/LDAPkey.pem -days 365 -subj "/C=$Co/ST=$St/L=$ci/O=$Org/OU=$O/CN=$Ca"
-							chown -R ldap:ldap /etc/openldap/certs/*.pem			
+							#changing the permitions
+							chown -R ldap:ldap /etc/openldap/certs/*.pem	
+						#dialog box
 						} | whiptail --title "LDAP configuration" --msgbox "certifications created" 10 60
 						{
-
+							#copy the schemas to the Ldap directory
 							cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
+							#changing the permitions
 							chown ldap:ldap /var/lib/ldap/*
-
+							
+							#dialog box with a loading bar
 				                        for ((i = 0 ; i <= 100 ; i+=20)); do
                                 				if [ $i = 20 ]; then
-                               				        	ldapmodify -Y EXTERNAL  -H ldapi:/// -f /tmp/LDAP.cfg/db.ldif
+									ldapmodify -Y EXTERNAL  -H ldapi:/// -f /tmp/LDAP.cfg/db.ldif
 								elif [ $i = 40 ]; then
                                 			        	ldapmodify -Y EXTERNAL  -H ldapi:/// -f /tmp/LDAP.cfg/certs.ldif
                                					elif [ $i = 60 ]; then
@@ -162,10 +195,12 @@ please introduce the two initial of the country. for example:" 10 60 US 3>&1 1>&
 								fi
                                 				sleep 1
 								done
+						#dialog box
                 				} | whiptail --gauge "Please wait while the configuration finish" 6 60 0				
 					fi
 				fi
 			fi
+		#dialog box
 		whiptail --title "LDAP configuration" --msgbox "Program Finished" 10 60
 
 	elif [ $Menu = 3 ]; then
