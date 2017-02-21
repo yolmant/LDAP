4adda7c981532b885f930ba8e5c18e6990c812b6
Yolman Torrez <yolmat24@outlook.com>
2017-02-20 21:53:11 -0800

    Update Ldap_client_Installer.bash

--- Installer/Ldap_client_Installer.bash
+++ Installer/Ldap_client_Installer.bash
@@ -1,22 +1,54 @@
 #!/bin/bash
 {
-	apt-get --yes update && apt-get --yes upgrade && apt-get --yes dist-upgrade
-
-} | whiptail --title "LDAP client" --msgbox "Updating the system to the lastest version. wait a minute" 10 60
-
-export DEBIAN_FRONTEND=noninteractive
-
-{
-	apt-get --yes install libpam-ldap ncsd
-	unset DEBIAN_FRONTEND
-
-} | whiptail --title "LDAP client" --msgbox "Installing Ldap and authentication client package" 10 60
+	for ((i = 0 ; i <= 100 ; i+=20)); do
+        	if [ $i = 20 ]; then
+			apt-get --yes update
+		elif [ $i = 40 ]; then
+			apt-get --yes upgrade
+		elif [ $i = 60 ]; then
+			apt-get --yes dist-upgrade
+		elif [ $i = 80 ]; then
+			export DEBIAN_FRONTEND=noninteractive
+		elif [ $i = 100 ]; then
+			apt-get --yes install libpam-ldap ncsd
+			unset DEBIAN_FRONTEND
+		fi
+		echo $i
+		sleep 1
+ 	done 	
+} | whiptail --title "LDAP client" --gauge "Please wait while installing" 6 60 0
 
 Domain=$(whiptail --title "LDAP client" --inputbox "introduce the LDAP domain. for example:" 10 60 dc=example,dc=net 3>&1 1>&2 2>&3)
 
 Ips=$(whiptail --title "LDAP client" --inputbox "introduce the server IP:" 10 60 3>&1 1>&2 2>&3)
 
-#modify ldap.conf 
-sed -i "s/base dc=example,dc=net/base $Domain/" /etc/ldap.conf
-sed -i "s/uri ldapi:\/\/\//uri ldap://$Ips\//" /etc/ldap.conf
-sed -i 's,rootbinddn cn=manager\,dc=example\,dc=net,#rootbinddn cn=manager\,dc=example\,dc=net,g' /etc/ldap.conf
+{
+	#modify ldap.conf 
+	sed -i "s/base dc=example,dc=net/base $Domain/" /etc/ldap.conf
+	sed -i "s/uri ldapi:\/\/\//uri ldap://$Ips\//" /etc/ldap.conf
+	sed -i 's,rootbinddn cn=manager\,dc=example\,dc=net,#rootbinddn cn=manager\,dc=example\,dc=net,g' /etc/ldap.conf
+
+	#modify nsswitch.conf	
+	sed -i 's,passwd:         compat,passwd:     ldap compat,g' /etc/nsswitch.conf 
+	sed -i 's,group:          compat,group:      ldap compat,g' /etc/nsswitch.conf
+	sed -i 's,shadow:         compat,shadow:     ldap compat,g' /etc/nsswitch.conf
+	sed -i 's,netgroup:       nis,netgroup:       ldap,g' /etc/nsswitch.conf
+
+	#modify common-session file
+	sed -i '$ a\session required      pam_mkhomedir.so skel=/etc/skel umask=0022' /etc/pam.d/common-session
+	
+	#restarting the nscd service
+	/etc/init.d/nscd restart
+} | whiptail --title "LDAP client" --msgbox "Installing LDAP configuration" 10 60
+
+{
+	#to allow the access of the instance with ssh
+	#comment out the next lines
+	sed -i 's,PasswordAuthentication no,#PasswordAuthentication no,g' /etc/ssh/sshd_config
+	sed -i 's,ChallengeResponseAuthentication no,#ChallengeResponseAuthentication no,g' /etc/ssh/sshd_config
+
+	#restart the sshd service
+	systemctl restart sshd.service
+} | whiptail --title "LDAP client" --msgbox "Configuring the SSH access" 10 60
+
+whiptail --title "LDAP clien" --msgbox "LDAP client installed" 10 60
