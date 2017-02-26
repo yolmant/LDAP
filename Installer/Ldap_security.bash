b54df6933b40c4c0dde41af710ac439fb5980e4d
root <root@ldap-server.c.project-x-154804.internal>
2017-02-26 07:14:55 +0000

    secure phpldapadmin webside

---
+++ Installer/Ldap_security.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+sed -i -e "s/\/\/\$servers->setValue('login','anon_bind',true);/\$servers->setValue('login','anon_bind',false);/" config.php
+
+yum install mod_ssl
+
+mkdir /etc/ssl/private
+
+chmod 700 /etc/ssl/private
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache.key -out /etc/ssl/certs/apache.crt
+
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
+sed -i "57s/Alias \/phpldapadmin \/usr\/share\/phpldapadmin\/htdocs\nAlias \/ldapadmin \/usr\/share\/phpldapadmin\/htdocs\nDocumentRoot \"\/usr\/share\/phpldapadmin\/htdocs\"/" /etc/httpd/conf.d/ssl.conf
+
+sed -i -e "s/SSLProtocol all -SSLv2/#SSLProtocol all -SSLv2/" /etc/httpd/conf.d/ssl.conf
+
+sed -i -e "s/SSLCipherSuite HIGH:MEDIUM:\!aNULL:\!MD5:\!SEED:\!IDEA/#SSLCipherSuite HIGH:MEDIUM:\!aNULL:\!MD5:\!SEED:\!IDEA/" /etc/httpd/conf.d/ssl.conf
+
+sh -c 'echo "# Begin copied text
+# from https://cipherli.st/
+# and https://raymii.org/s/tutorials/Strong_SSL_Security_On_Apache2.html
+
+SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH
+SSLProtocol All -SSLv2 -SSLv3
+SSLHonorCipherOrder On
+# Disable preloading HSTS for now.  You can use the commented out header line that includes
+# the \"preload\" directive if you understand the implications.
+#Header always set Strict-Transport-Security \"max-age=63072000; includeSubdomains; preload\"
+Header always set Strict-Transport-Security \"max-age=63072000; includeSubdomains\"
+Header always set X-Frame-Options DENY
+Header always set X-Content-Type-Options nosniff
+# Requires Apache >= 2.4
+SSLCompression off
+SSLUseStapling on
+SSLStaplingCache \"shmcb:logs/stapling-cache(150000)\"
+# Requires Apache >= 2.4.11
+# SSLSessionTickets Off" >> /etc/httpd/conf.d/ssl'
+
+systemctl restart httpd.service
