aa4c4f3c13f464dcccef7600aa4d96d050ef8911
Yolman Torrez <yolmat24@outlook.com>
2017-02-25 23:29:59 -0800

    Update Ldap_security.bash

--- Installer/Ldap_security.bash
+++ Installer/Ldap_security.bash
@@ -1,23 +1,28 @@
 #!/bin/bash
-
+#disable the anonymous login
 sed -i -e "s/\/\/\$servers->setValue('login','anon_bind',true);/\$servers->setValue('login','anon_bind',false);/" config.php
 
+#instal SSL
 yum install mod_ssl
 
+#create a new directory to store the key
 mkdir /etc/ssl/private
 
+#change the permission of the new directory
 chmod 700 /etc/ssl/private
 
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache.key -out /etc/ssl/certs/apache.crt
-
+#creating the certification and key
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache.key -out /etc/ssl/certs/apache.crt
 openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
+#include the detail of the phpldapadmin webside to the port 443
 sed -i "57s/Alias \/phpldapadmin \/usr\/share\/phpldapadmin\/htdocs\nAlias \/ldapadmin \/usr\/share\/phpldapadmin\/htdocs\nDocumentRoot \"\/usr\/share\/phpldapadmin\/htdocs\"/" /etc/httpd/conf.d/ssl.conf
 
+#comment out
 sed -i -e "s/SSLProtocol all -SSLv2/#SSLProtocol all -SSLv2/" /etc/httpd/conf.d/ssl.conf
-
 sed -i -e "s/SSLCipherSuite HIGH:MEDIUM:\!aNULL:\!MD5:\!SEED:\!IDEA/#SSLCipherSuite HIGH:MEDIUM:\!aNULL:\!MD5:\!SEED:\!IDEA/" /etc/httpd/conf.d/ssl.conf
 
+#include the Update Cypher suit
 sh -c 'echo "# Begin copied text
 # from https://cipherli.st/
 # and https://raymii.org/s/tutorials/Strong_SSL_Security_On_Apache2.html
@@ -38,4 +43,5 @@ SSLStaplingCache \"shmcb:logs/stapling-cache(150000)\"
 # Requires Apache >= 2.4.11
 # SSLSessionTickets Off" >> /etc/httpd/conf.d/ssl'
 
+#Restart the hhtpd service
 systemctl restart httpd.service
